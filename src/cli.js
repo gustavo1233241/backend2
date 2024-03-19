797b97d8a5030c05b272f3a7bcb57e3bb0bebf0b
gu <gusera4325@gmail.com>
2024-03-19 16:05:52 -0300

    alterei

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,32 @@
 
 import pegaArquivo  from "./index.js";
+import fs from "fs"
+import chalk from "chalk";
 const caminho=process.argv;
 
 console.log(caminho)
 
-pegaArquivo(caminho[2])
+//pegaArquivo(caminho[2])
+
+async function processaTexto(argumentos){
+    const caminho= argumentos[2];
+if(fs.lstatSync(caminho).isFile()){
+
+    const resultado = await pegaArquivo(caminho);
+    console.log(chalk.yellow('lista resultados'), resultado);
+}
+else if(fs.lstatSync(caminho).isDirectory()){
+
+    const arquivos = await fs.promises.readdir(caminho);
+
+    arquivos.forEach(async (nomeDoArquivo)=> {
+     const lista = await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
+     console.log(lista);
+     console.log(`${caminho}/${nomeDoArquivo}`);
+    });
+
+
+}
+
+}
+processaTexto(caminho);
--- src/index.js
+++ src/index.js
@@ -1,104 +1,102 @@
-import fs from 'fs';
+
+import fs, { realpath } from 'fs';
 
 import chalk from 'chalk';
-import { error } from 'console';
 
+const regex = '\[[^[\]]*?]\(https?:\/\/[^\s?#.]*[^\s]*\)';
 
+//const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
 
+function extraiLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
 
-//const textoTest="São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações"
+    //const capturas = texto.match(regex);
+    //const capturas = regex.exec(texto);
+    //const capturas = texto.matchAll(regex);
 
+    //operador de espalhamento
+    const capturas =[...texto.matchAll(regex)];
 
-function extraLinks(texto){
+   /*for(let i = 0; i < capturas.length; i++){
+        console.log(capturas[i][2]);
+    }*/
 
-
-    const regex = /\[([^[\]]*?)]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
-
-   // const capturas = regex.exec(texto)
-    const capturas = [...texto.matchAll(regex)];
-
-
-const resultados=capturas.map(capturas=>({[capturas[1]]:capturas[2]}))
-
-   // console.log(capturas);
-
-return resultados
-   //console.log(resultados);
+    const resultados = capturas.map(captura => ({[captura[1]]:captura[2]}))
+    //console.log(resultados);
+    return resultados.length !== 0?resultados:'Não há links no arquivo';
 }
 
+//extraiLinks(textoTeste);
+/*
+console.log(chalk.blue('Olá mundo'));
 
-//extraLinks(textoTest)
+console.log(chalk.blue.bgWhite.bold("SENAI"));
 
-// console.log(chalk.blue('Olá mundo'));
+console.log(chalk.blue('Curso',' de',' Node.JS'));
 
-// console.log(chalk.blue.bgWhite.bold("SENAI"));
+console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
 
-// console.log(chalk.blue('Curso',' de',' Node.JS'));
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+*/
 
-// console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+//2) tratamento de erros, função para tratar os errosd
 
-// console.log(`
-// CPU: ${chalk.red('90%')}
-// RAM: ${chalk.green('40%')}
-// DISK: ${chalk.yellow('70%')}
-// `);
-
-
-
- function trataErro(erro){
-
-console.log(erro)
- throw new Error(chalk.red(erro.code, "não ha arquivo no diretorio"))
-
- }
+function trataErro(erro){
+    console.log(erro);
+    throw new Error(chalk.red(erro.code,'não a arquivo no diretório'));
+}
+/*
 
 
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding="utf-8";
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
 
-// if(erro){
+//1) escrever uma function que traga arquivos .md
 
-//     trataErro(erro)
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    });
+}
 
-// }
+pegaArquivo('./arquivos/');
+//callback(erro, texto)
+*/
 
-//         console.log(chalk.green(texto));
- 
- 
-//     })
-// }
-// pegaArquivo('./arquivos/')
+//aula sobre Promessas, inserir método assincrono no nossa código
 
-/*function trataErro(erro){
+//1) reescrevendo (refatorando)
 
-    console.log(erro)
-     throw new Error(chalk.red(erro.code, "não ha arquivo no diretorio"))
-    
-     }
+/*
+function pegaArquivo(caminhoDoArquivo){
+    const enconding = 'utf-8';
+    fs.promises.readFile(caminhoDoArquivo, enconding).then((texto) => console.log(chalk.yellow(texto))).catch((erro) => trataErro(erro));
+}
+pegaArquivo('./arquivos/');
+*/
 
-*/ async function pegaArquivo(caminhoDoArquivo){
+//usando outra forma de solucionar as promessas (async/await)
 
-    try{
-
-        const encoding="utf-8"
-        const texto= await fs.promises.readFile(caminhoDoArquivo,encoding)
-    console.log(extraLinks(texto))
-
+async function pegaArquivo(caminhoDoArquivo){
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        //console.log(chalk.blue(texto));
+        //console.log(extraiLinks(texto));
+        return extraiLinks(texto);
+    } catch(erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.magenta('Operação concluida'));
     }
-catch{(erro)
-
-
-    trataErro(erro)
-}finally{
-
-console.log(chalk.cyan("Operação concluida"))
 }
-
-
-}
-
-
 export default pegaArquivo;
-//pegaArquivo("./arquivos/texto.md");
-// pegaArquivo("./arquivos/");
+//pegaArquivo('./arquivos/texto.md');
+//pegaArquivo('./arquivos/');
+
