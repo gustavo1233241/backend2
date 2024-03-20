4f583cb1e3bab6c1a09fecba69b38573374fa6d1
Gus <gusera4325@gmail.com>
2024-03-20 14:42:44 -0300

    alterei isso

--- src/cli.js
+++ src/cli.js
@@ -1,32 +1,67 @@
 
-import pegaArquivo  from "./index.js";
+import pegaArquivo from "./index.js";
 import fs from "fs"
 import chalk from "chalk";
-const caminho=process.argv;
+const caminho = process.argv;
 
 console.log(caminho)
 
+
+
+function imprimeLista(resultado, identigacador='') {
+
+    console.log(chalk.yellow('lista resultados'), 
+    chalk.black.bgCyan(identigacador), 
+    resultado);
+
+}
+
 //pegaArquivo(caminho[2])
 
-async function processaTexto(argumentos){
-    const caminho= argumentos[2];
-if(fs.lstatSync(caminho).isFile()){
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
 
-    const resultado = await pegaArquivo(caminho);
-    console.log(chalk.yellow('lista resultados'), resultado);
+
+
+    try{
+
+
+        fs.statSync(caminho)
+
+
+    }
+    catch(erro){
+
+if(erro.code === "ENOENT"){
+
+    console.log("Arquivo ou diretorio não encontrado");
+    return;
 }
-else if(fs.lstatSync(caminho).isDirectory()){
+      
+    }
 
-    const arquivos = await fs.promises.readdir(caminho);
+    if (fs.lstatSync(caminho).isFile()) {
 
-    arquivos.forEach(async (nomeDoArquivo)=> {
-     const lista = await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
-     console.log(lista);
-     console.log(`${caminho}/${nomeDoArquivo}`);
-    });
+        const resultado = await pegaArquivo(caminho);
+        //console.log(chalk.yellow('lista resultados'), resultado);
+        imprimeLista(resultado)
+    }
+    else if (fs.lstatSync(caminho).isDirectory()) {
 
+        const arquivos = await fs.promises.readdir(caminho);
+
+        arquivos.forEach(async (nomeDoArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
+            // console.log(lista);
+            // console.log(`${caminho}/${nomeDoArquivo}`);
+            imprimeLista(lista,nomeDoArquivo);
+        });
+
+
+    }
 
 }
 
-}
+export default pegaArquivo;
+//console.log("oi")
 processaTexto(caminho);
